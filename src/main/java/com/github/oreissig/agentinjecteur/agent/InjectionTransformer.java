4745b83ee6cd70460841b8cd9231c5313c28da5e
oreissig <oreissig+github@gmail.com>
2016-08-20 00:48:33 +0200

    check that there is no write to an injected field

--- src/main/java/com/github/oreissig/agentinjecteur/agent/InjectionTransformer.java
+++ src/main/java/com/github/oreissig/agentinjecteur/agent/InjectionTransformer.java
@@ -2,6 +2,8 @@ package com.github.oreissig.agentinjecteur.agent;
 
 import com.github.oreissig.agentinjecteur.rt.MembersInjector;
 import javassist.*;
+import javassist.expr.ExprEditor;
+import javassist.expr.FieldAccess;
 
 import javax.inject.Inject;
 import java.io.ByteArrayInputStream;
@@ -11,6 +13,7 @@ import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 import static javassist.Modifier.FINAL;
+import static javassist.Modifier.PRIVATE;
 
 class InjectionTransformer {
 
@@ -36,9 +39,7 @@ class InjectionTransformer {
     private void injectMembers(CtClass clazz, List<CtField> injectTargets) {
         try {
             for (CtField f : injectTargets) {
-                // make field final
-                // TODO check that there is no write to an injected field
-                f.setModifiers(f.getModifiers() | FINAL);
+                makeFinal(f);
                 // TODO replace @javax.inject.Inject by proprietary @Injected annotation to avoid double injection
 
                 String typeName = f.getType().getName();
@@ -48,6 +49,26 @@ class InjectionTransformer {
             }
         } catch (NotFoundException | CannotCompileException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void makeFinal(CtField f) throws CannotCompileException {
+        if ((f.getModifiers() & PRIVATE) > 0) {
+            // check that there is no write to an injected field
+            f.getDeclaringClass().instrument(new ExprEditor() {
+                public void edit(FieldAccess fa) throws CannotCompileException {
+                    try {
+                        if (f.equals(fa.getField()) && fa.isWriter()) {
+                            throw new CannotCompileException("write to private injected field " +
+                                    f.getName() + " not allowed");
+                        }
+                    } catch (NotFoundException e) {
+                        throw new CannotCompileException(e);
+                    }
+                }
+            });
+            // make field final
+            f.setModifiers(f.getModifiers() | FINAL);
         }
     }
 }
